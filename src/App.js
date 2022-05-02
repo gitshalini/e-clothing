4a946b7fb5b90d8f02717caee407b891336b8391
shalini.gupta <shalini.gupta@bold.com>
2022-05-02 15:26:22 +0530

    create new folder and files and make a component accordingly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import HomePage from './Homepage.component';
+import HomePage from './pages/Home/Homepage.component';
 
 
 function App() {
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size}) =>{
+    return(
+        <div className={`${size} menu-item`} style={{
+            backgroundImage : `url(${imageUrl})`
+        }}>
+          <div className='content'>
+            <div className='title'>{title}</div>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+    );
+}
+export default MenuItem;
