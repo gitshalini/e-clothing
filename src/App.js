20f72c5d1f2ec129b5eac6c9c715ed08fb0be4d3
shalini.gupta <shalini.gupta@bold.com>
2022-05-06 11:40:05 +0530

    install react router and implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import HomePage from './pages/Home/Homepage.component';
-
+import { Routes, Route } from "react-router-dom";
+const HatsPage = () =>{
+  return(
+    <div><h1>HATS</h1></div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/hats" element={<HatsPage/>}></Route>
+      </Routes>
     </div>
   );
 }
