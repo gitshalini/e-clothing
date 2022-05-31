6b55146c68c6620399eef220ec1888583afe5f08
shalini.gupta <shalini.gupta@bold.com>
2022-05-31 15:09:55 +0530

    update our menu-item to navigate dynamically by using version 6 new fn useNavigate that is the replacement of withRouters fn

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ const HatsPage = () =>{
     <div><h1>HATS</h1></div>
   );
 }
+const JacketsPage = () =>{
+  return(
+    <div><h1>JacketPage</h1></div>
+  );
+}
+const SneakersPage = () =>{
+  return(
+    <div><h1>SneakersPage</h1></div>
+  );
+}
+const WomensPage = () =>{
+  return(
+    <div><h1>WomensPage</h1></div>
+  );
+}
 
 function App() {
   return (
@@ -13,6 +28,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/hats" element={<HatsPage/>}></Route>
+        <Route path="/jackets" element={<JacketsPage/>}></Route>
+        <Route path="/sneakers" element={<SneakersPage/>}></Route>
+        <Route path="/womens" element={<WomensPage/>}></Route>
       </Routes>
     </div>
   );
