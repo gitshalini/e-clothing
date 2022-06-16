795d88fda107937763de470e1e9ec632708152ae
shalini.gupta <shalini.gupta@bold.com>
2022-06-16 11:43:48 +0530

    adding headercomponent and its menus

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import HomePage from './pages/Home/Homepage.component';
 import { Routes, Route } from "react-router-dom";
 import ShopPage from './pages/Shoppage/shopPage.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/shop" element={<ShopPage/>}></Route>
